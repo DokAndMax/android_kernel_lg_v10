c0e0dceefb38b6fd2e089f5a9bc3c6da4bba7ea9
Maxim <maxim.kryvenok@gmail.com>
2018-07-22 14:17:16 +0300

    dsi_drv switch to mdelay

--- drivers/misc/mediatek/video/mt6582/dsi_drv.c
+++ drivers/misc/mediatek/video/mt6582/dsi_drv.c
@@ -3316,7 +3316,7 @@ void DSI_set_cmdq_V3(LCM_setting_table_V3 *para_tbl, unsigned int size, unsigned
 
 		if (data_id == REGFLAG_ESCAPE_ID && cmd == REGFLAG_DELAY_MS_V3)
 		{
-			udelay(1000*count);
+			mdelay(1*count);
 			DISP_LOG_PRINT(ANDROID_LOG_INFO, "DSI", "DSI_set_cmdq_V3[%d]. Delay %d (ms) \n", index, count);
 
 			continue;
